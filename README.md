031089691371b2449ce58a6655bc8e7aca44744c
winniehell <git@winniehell.de>
2016-08-17 10:08:33 +0200

    add test files

---
+++ README.md
@@ -0,0 +1,1 @@
+Test for https://github.com/steel/karma-sprockets-mincer/pull/11
---
+++ spec/assets/javascripts/application_spec.js
@@ -0,0 +1,8 @@
+//= require application
+//= require some-library
+
+describe('Just a', function() {
+  it('test', function() {
+    console.log('Nothing special here...')
+  })
+})
